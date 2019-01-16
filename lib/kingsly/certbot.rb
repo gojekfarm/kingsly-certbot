1cab935c9ffbf4eb67b8a61345fba880b8fafeb9
Tasdik Rahman <tasdik.rahman@go-jek.com>
2019-01-16 13:09:39 +0530

    git init

---
+++ lib/kingsly/certbot.rb
@@ -0,0 +1,8 @@
+require "kingsly/certbot/version"
+
+module Kingsly
+  module Certbot
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/kingsly/certbot_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Kingsly::Certbot do
+  it "has a version number" do
+    expect(Kingsly::Certbot::VERSION).not_to be nil
+  end
+end
